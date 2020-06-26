6b86a2a7e7b85f43e00ec199ed5cab3552e9ebda
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.com>
2020-06-26 20:23:26 +0200

    feat: add zh-TW translation

---
+++ Assets/RPS/Scripts/TextTranslator.cs
@@ -0,0 +1,20 @@
+﻿using ngettext_unity;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public sealed class TextTranslator : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+
+    private void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+
+        NGettextUnity.Instance.LoadLocale("zh-TW");
+        Debug.Log(text.text);
+        text.text = NGettextUnity.Catalog.GetString(text.text);
+        Debug.Log(text.text);
+    }
+}

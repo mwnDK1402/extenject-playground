b527931fb03436995515dcea99c953790b519301
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.com>
2020-06-29 13:55:24 +0200

    feat: add CountdownSlider
    Set up GameObjects for a UI state machine.
    Added arguments to TextTranslator.
    Expanded zh-TW char set to include play button and countdown slider.

---
+++ Assets/RPS/Scripts/RPS/CountdownSlider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UGettext;
+using UnityEngine.UI;
+using Zenject;
+
+namespace RPS
+{
+    internal sealed class CountdownSlider : IInitializable, IDisposable
+    {
+        private I18n i18n;
+        private Slider slider;
+        private TextTranslator text;
+        private string originalText;
+
+        public float CountdownTime => slider.value;
+
+        [Inject]
+        public CountdownSlider(I18n i18n, Slider slider, TextTranslator text)
+        {
+            this.i18n = i18n;
+            this.slider = slider;
+            this.text = text;
+            originalText = text.GetOriginalText();
+        }
+
+        void IDisposable.Dispose()
+        {
+            slider.onValueChanged.RemoveListener(OnValueChanged);
+        }
+
+        void IInitializable.Initialize()
+        {
+            UpdateText();
+            slider.onValueChanged.AddListener(OnValueChanged);
+        }
+
+        private void OnValueChanged(float value)
+        {
+            UpdateText();
+        }
+
+        private void UpdateText()
+        {
+            text.SetOriginalText(originalText, slider.value);
+        }
+    }
+}
--- Assets/RPS/Scripts/RPS/Language/TextTranslator.cs
+++ Assets/RPS/Scripts/RPS/Language/TextTranslator.cs
@@ -12,11 +12,24 @@ namespace RPS
         private I18n i18n;
         private string originalText;
         private TextMeshProUGUI text;
+        private object[] arguments;
+
+        public string GetOriginalText() => originalText;
+
+        public void SetOriginalText(string originalText, params object[] args)
+        {
+            this.originalText = originalText;
+            arguments = args;
+            UpdateText();
+        }
 
         [Inject]
         private void Construct(I18n i18n)
         {
             this.i18n = i18n;
+            text = GetComponent<TextMeshProUGUI>();
+            originalText = text.text;
+            arguments = new object[0];
         }
 
         private void OnDestroy()
@@ -31,15 +44,13 @@ namespace RPS
 
         private void Start()
         {
-            text = GetComponent<TextMeshProUGUI>();
-            originalText = text.text;
             UpdateText();
             i18n.LanguageChanged += OnLanguageChanged;
         }
 
         private void UpdateText()
         {
-            text.text = i18n.Catalog.GetString(originalText);
+            text.text = i18n.Catalog.GetString(originalText, arguments);
         }
     }
 }
